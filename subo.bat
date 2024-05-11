2521ef31140bc6ef7b39b083ed1c79ec95683dc5
ArnoldMunozC <maomauricioc@gmail.com>
2024-05-10 21:50:04 -0500

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -5,6 +5,6 @@ git push
 set GOOS=linux
 set GOARCH=amd64
 
-go build -tags lambda.norpc -o bootstrap.main.go
+go build -tags lambda.enospc -o bootstrap.main.go
 del main.zip
 tar.exe -a -cf main.zip main
