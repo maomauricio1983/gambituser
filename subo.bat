7f756ebf03c4301ba18b966ce6473416bb92b899
ArnoldMunozC <maomauricioc@gmail.com>
2024-05-14 15:09:41 -0500

    last Commit

--- subo.bat
+++ subo.bat
@@ -1,10 +1,10 @@
 git add .
-git commit -m "Ultimo Commit"
+git commit -m "last Commit"
 git push
 
 set GOOS=linux
 set GOARCH=amd64
 
-go build main.go
-del main.zip
-zip main.zip main
+go build -o bootstrap main.go
+del bootstrap.zip
+tar.exe -a -cf bootstrap.zip bootstrap
